508345347ca2f87106d263a282181d91c8eb9f70
Ranjithdurai451 <ranjithdurai111@gmail.com>
2025-04-06 17:03:36 +0000

    update action.ts file on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,23 @@
-
-import { Suspense } from "react";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./components/ThemeProvider";
-import InitialLoader from "./components/InitialLoader";
-import Index from "./pages/Index";
-import Dashboard from "./pages/Dashboard";
-import RoadmapResult from "./pages/RoadmapResult";
-import ChecklistItemDetail from "./pages/ChecklistItemDetail";
-import LegalChecklistItemDetail from "./pages/LegalChecklistItemDetail";
-import NotFound from "./pages/NotFound";
+import { Suspense, useEffect } from 'react';
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './components/ThemeProvider';
+import InitialLoader from './components/InitialLoader';
+import Index from './pages/Index';
+import Dashboard from './pages/Dashboard';
+import RoadmapResult from './pages/RoadmapResult';
+import ChecklistItemDetail from './pages/ChecklistItemDetail';
+import LegalChecklistItemDetail from './pages/LegalChecklistItemDetail';
+import NotFound from './pages/NotFound';
 
 const queryClient = new QueryClient();
+
+useEffect(() => {
+  fetch(import.meta.env.VITE_BACEKND_BASE_URL + '/api');
+}, []);
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -30,8 +33,14 @@ const App = () => (
                 <Route path="/" element={<Index />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/roadmap-result" element={<RoadmapResult />} />
-                <Route path="/checklist-item/:itemTitle" element={<ChecklistItemDetail />} />
-                <Route path="/legal-checklist-item/:itemTitle" element={<LegalChecklistItemDetail />} />
+                <Route
+                  path="/checklist-item/:itemTitle"
+                  element={<ChecklistItemDetail />}
+                />
+                <Route
+                  path="/legal-checklist-item/:itemTitle"
+                  element={<LegalChecklistItemDetail />}
+                />
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </div>
